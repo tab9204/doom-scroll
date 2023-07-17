b8e906b01de32cb70abcf6ac0576ee9dea161a49
tab9204 <tab9204@g.rit.edu>
2023-07-17 14:01:08 -0700

    fixed login page styling

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -7,7 +7,7 @@
     const authorizeURL = `https://www.reddit.com/api/v1/authorize.compact?client_id=J35OkEqpyYR5KwrMP8kSFA&response_type=code&state=${state}&redirect_uri=${redirect}&duration=permanent&scope=mysubreddits,read`;
 </script>
 
-<main class="container text-center">
+<div class="text-center">
     <h1 class="text-2xl mb-5">Welcome to RedditKit</h1>
     <a class="text-tertiary-500" href="{authorizeURL}" target="_top">Sign in with your Reddit account</a>
-</main>
+</div>
