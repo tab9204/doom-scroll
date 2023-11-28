7559cf6723a6439663ca6af789f507143d4aa7ba
tab9204 <tab9204@g.rit.edu>
2023-11-28 09:27:43 -0800

    fixed generic post content

--- src/lib/components/content_types/Generic_Content.svelte
+++ src/lib/components/content_types/Generic_Content.svelte
@@ -2,6 +2,7 @@
     import Image_Content from "$lib/components/content_types/Image_Content.svelte";
     import Video_Content from "$lib/components/content_types/Video_Content.svelte";
     import Embedded_Content from "$lib/components/content_types/Embedded_Content.svelte";
+    import Link_Content from "$lib/components/content_types/Link_Content.svelte";
 
     export let post;
 </script>
@@ -11,8 +12,8 @@
     <Embedded_Content content={post.embed}></Embedded_Content>
 {:else if post.video}
     <Video_Content video={post.video}></Video_Content>
-{/if}
-
-{#if post.images.length > 0}
+{:else if post.images.length > 0}
     <Image_Content images={post.images}></Image_Content>
+{:else if post.link}
+    <Link_Content link={post.link}></Link_Content>
 {/if}
